4f75a4795d760b3a5002462abb01a09716cd3f15
moustafa <moustafaroushdy1@gmail.com>
2022-07-04 14:25:29 +0200

    attachment added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,16 +19,33 @@ class BooksController < ApplicationController
         @book = Book.new(book_params)
         @book.save!
         if @book.save!
-            redirect_to @book
+            redirect_to books_path
         else 
             render :new , status: :unprocessable_entity
         end
     end
 
+    def edit 
+        @book = Book.find(params[:id])
+    end
+
+    def update
+        @book = Book.find(params[:id])
+
+        if @book.update(book_params)
+            redirect_to @book
+        else
+            render :edit , status::unprocessable_entity
+        end
+    end
+            
+
+
+
     private
 
         def book_params
-            params.require(:book).permit(:name,:price,:description,:admin_id)
+            params.require(:book).permit(:name,:price,:description,:admin_id,:image)
           
         end
 
